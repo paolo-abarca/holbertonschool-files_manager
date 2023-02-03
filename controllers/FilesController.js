4674ae30d43cc363da70dc500ae69c3706d4906b
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-02-03 17:25:36 -0500

    Adding FilesController.js file that add the 2 new endpoints

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -101,6 +101,112 @@ class FilesController {
       parentId: fileInsertData.parentId,
     });
   }
+
+  static async getShow(req, res) {
+    async function getIdKey(req) {
+      const userInfo = { userId: null, key: null };
+
+      const token = req.header('X-Token');
+      if (!token) return userInfo;
+
+      userInfo.key = `auth_${token}`;
+      userInfo.userId = await redisClient.get(userInfo.key);
+
+      return userInfo;
+    }
+
+    const { userId } = await getIdKey(req);
+
+    function isValidUser(id) {
+      try {
+        ObjectId(id);
+      } catch (error) {
+        return false;
+      }
+      return true;
+    }
+
+    if (!isValidUser(userId)) return res.status(401).send({ error: 'Unauthorized' });
+
+    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    const argId = req.params.id;
+    const file = await dbClient.users.findOne({ _id: ObjectId(argId), userId: user._id });
+    if (!file) return res.status(404).send({ error: 'Not found' });
+
+    return res.status(200).send({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
+  }
+
+  static async getIndex(req, res) {
+    async function getIdKey(req) {
+      const userInfo = { userId: null, key: null };
+
+      const token = req.header('X-Token');
+      if (!token) return userInfo;
+
+      userInfo.key = `auth_${token}`;
+      userInfo.userId = await redisClient.get(userInfo.key);
+
+      return userInfo;
+    }
+
+    const { userId } = await getIdKey(req);
+
+    function isValidUser(id) {
+      try {
+        ObjectId(id);
+      } catch (error) {
+        return false;
+      }
+      return true;
+    }
+
+    if (!isValidUser(userId)) return res.status(401).send({ error: 'Unauthorized' });
+
+    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    let parentId = req.query.parentId || 0;
+    if (parentId === '0') parentId = 0;
+    if (parentId !== 0) {
+      if (!isValidUser(parentId)) return res.status(401).send({ error: 'Unauthorized' });
+      parentId = ObjectId(parentId);
+
+      const folder = await dbClient.files.findOne({ _id: ObjectId(parentId) });
+      if (!folder || folder.type !== 'folder') return res.status(200).send([]);
+    }
+
+    const page = req.query.page || 0;
+
+    const addmath = { $and: [{ parentId }] };
+    let data = [{ $match: addmath }, { $skip: page * 20 }, { $limit: 20 }];
+    if (parentId === 0) data = [{ $skip: page * 20 }, { $limit: 20 }];
+
+    const pagination = await dbClient.files.aggregate(data);
+    const paginationArray = [];
+
+    await pagination.forEach((file) => {
+      const paginationItem = {
+        id: file._id,
+        userID: file.userId,
+        name: file.name,
+        type: file.type,
+        isPublic: file.isPublic,
+        parentId: file.parentId,
+      };
+      paginationArray.push(paginationItem);
+    });
+
+    return res.status(200).send(paginationArray);
+  }
 }
 
 export default FilesController;
