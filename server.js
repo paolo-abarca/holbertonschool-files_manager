9a9d4ec96657e7f49df7c04faf7d46f3307122b9
gpradinett <gpradinett@gmail.com>
2023-02-02 20:28:20 -0500

    ♻️ correct import method

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import router from './routes/index';
-
-const express = require('express');
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(router);
+app.use(express.json());
+
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
