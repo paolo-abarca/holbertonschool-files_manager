f8fdacfb1847308da5ceafe022fff5d670b43445
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-02-02 20:22:55 -0500

    Updating db.js file that contains the class DBClient

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,17 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
       if (!error) {
         this.db = client.db(database);
-        this.user = this.db.collection('users');
-        this.file = this.db.collection('files');
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
       } else {
         console.log(error.message);
         this.db = false;
@@ -24,11 +25,11 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.user.countDocuments();
+    return this.users.countDocuments();
   }
 
   async nbFiles() {
-    return this.file.countDocuments();
+    return this.files.countDocuments();
   }
 }
 
